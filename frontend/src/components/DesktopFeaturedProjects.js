2ea09af70c6f193e05baa50e94b0571e80710f54
thecodingconductor <tristan.raissherman@gmail.com>
2021-06-03 19:28:34 -0400

    working on the bottom of the desktop

--- frontend/src/components/DesktopFeaturedProjects.js
+++ frontend/src/components/DesktopFeaturedProjects.js
@@ -94,50 +94,14 @@ const DesktopFeaturedProjects = () => {
 
            <hr className="desktop-project-divider"></hr>
 
-           {/* <Container className="orchesnation-container" fluid>
-                <h4>OrchesNation</h4>
-                <p>
-                  Web application to assist orchestral professionals organize their contact lists, and view organizations' personnel. 
-                </p>
-                <img src={OrchesNation} alt="OrchesNation Home Page" className="orches-nation-image"></img>
-                <Container fluid className="view-project-button d-flex flex-row align-items-center justify-content-between">
-                    <p className="view-project">
-                        VIEW PROJECT
-                        <span className="line-through-work"></span>
-                    </p>
-                    <img src={ArrowMobile} alt="View Project"></img>
-                </Container>
-                <Container className="projects-tags-container" fluid>
-                    <Button className="project-tag">React Hooks</Button>
-                    <Button className="project-tag">ExpressJS, MongoDB</Button>
-                    <Button className="project-tag">REST API</Button>
-                </Container>
-           </Container> */}
-           {/* <Container className="mars-rover-container" fluid>
-                <h4>Mars Rover</h4>
-                <p>
-                   User can view randomly generated photos from the Mars Rovers and save favorites.
-                </p>
-                <img src={MarsRover} alt="Mars Rover Home Page" className="mars-rover-image"></img>
-                <Container fluid className="view-project-button d-flex flex-row align-items-center justify-content-between">
-                    <p className="view-project">
-                        VIEW PROJECT
-                        <span className="line-through-work"></span>
-                    </p>
-                    <img src={ArrowMobile} alt="View Project"></img>
-                </Container>
-                <Container className="projects-tags-container d-flex flex-column align-items-start" fluid>
-                    <Button className="project-tag">React Hooks</Button>
-                    <Button className="project-tag">Sass</Button>
-                </Container>
-           </Container> */}
+          
            <Container className="view-all-projects-container" fluid>
                 <img src={Can} alt="Can" className="can-image"></img>
                 <Container className="can-spacer" fluid>
 
                 </Container>
                 <Container className="view-projects-selector d-flex align-items-center justify-content-between" fluid>
-                    <p className="view-project d-flex flex-column align-items-center justify-content-center">
+                    <p className="view-project d-flex align-items-center justify-content-center">
                         <span className="view-all-text">VIEW ALL</span>
                         <span className="projects-text">PROJECTS</span> 
                     </p>
