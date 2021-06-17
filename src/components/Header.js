ed11a5e37c388615330978b35e531488f9c19c71
Tristan <tristan.raissherman@gmail.com>
2021-06-17 10:21:55 -0400

    added the dropdown effects

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,40 +10,27 @@ import useDelayUnmount from '../hooks/useDelayUnmount'
 
 const Header = (props) => {
 
-  // const navContext = useContext(NavContext);
-  // const {mobileNavAction, navOpen, testNavOpen} = navContext
-
-  // const [navOpen, setNavOpen] = useState(false);
   const [isMounted, setIsMounted] = useState(false);
   const showDiv = useDelayUnmount(isMounted, 250);
 
+  const [dropDownShow, setDropDownShow] = useState(false)
+
+  const showDropdown = e => {
+    setDropDownShow(!dropDownShow)
+  }
+
+  const hideDropdown = e => {
+    setDropDownShow(false)
+  }
+
   let history = useHistory();
 
-  // const mountedStyle = {animation: "inAnimation 250ms ease-in"}
-  // const unmountedStyle = {
-  //   animation: "outAnimation 270ms ease-out",
-  //   animationFillMode: "forwards"
-  // }
-
-  // const openMobileHandler = () => {
-    
-  //   navOpen ? mobileNavAction(false) : mobileNavAction(true)
-  // }
-
-  // const testOpenMobileHandler = () => {
-  //   testNavOpen
-  // }
 
   const returnHome = () => {
     history.push('/')
   }
 
 
-
-
-
-
-  // const [state, show, hide] = useDelayUnmount();
 
     return (
       <Container fluid className="navbar-main-mobile">
@@ -57,12 +44,14 @@ const Header = (props) => {
           
           <Container className="navbar-right d-flex flex-row align-items-center" fluid>
              <p className="mobile-title-right" onClick={returnHome}>Tristan Rais-Sherman</p>
+
               <Nav className="flex-row desktop-nav-wrapper justify-content-between">
       
-                <DropdownButton id="dropdown-basic-button" className="desktop-nav-button" title="Projects">
-                  <Dropdown.Item href="/concertbuilder">Concert Builder</Dropdown.Item>
-                  <Dropdown.Item href="/orchesnation">OrchesNation</Dropdown.Item>
-                  <Dropdown.Item href="/marsrover">Mars Rover</Dropdown.Item>
+                <DropdownButton id="dropdown-basic-button" show={dropDownShow} className="desktop-nav-button" title="Projects" onMouseEnter={showDropdown} onMouseLeave={hideDropdown}>
+                  <Dropdown.Item href="/concertbuilder" className="d-flex justify-content-start align-items-center">Concert Builder</Dropdown.Item>
+                  <Dropdown.Item href="/orchesnation" className="d-flex justify-content-start align-items-center">OrchesNation</Dropdown.Item>
+                  <Dropdown.Item href="/marsrover" className="d-flex justify-content-start align-items-center">Mars Rover</Dropdown.Item>
+                  <Dropdown.Item href="#" className="d-flex justify-content-start align-items-center">Mars Rover</Dropdown.Item>
                 </DropdownButton>
                 <Nav.Link className="desktop-nav-link" href="mailto: tristan.raissherman@gmail.com">Contact</Nav.Link>
                 <Nav.Link className="desktop-nav-link" href="https://www.twitch.tv/orchestraobsessed" target="_blank">Twitch</Nav.Link>
